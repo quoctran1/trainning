e8b00cb83a37abcb7eaf72b2bf4073822574c131
quoctran1 <87870359+quoctran1@users.noreply.github.com>
2025-03-18 08:43:42 +0700

    update: update code in RxDart

--- lib/screen/subject_screen/behaviour_subject_screen.dart
+++ lib/screen/subject_screen/behaviour_subject_screen.dart
@@ -52,30 +52,31 @@ class _BehaviourSubjectScreenState extends State<BehaviourSubjectScreen> {
 
   }
   /*
-+─────────────+───────────────────────────────────────────────────────────────────────────────+──────────────────+
-| action      | microtasks                                                                    | prints           |
-+─────────────+───────────────────────────────────────────────────────────────────────────────+──────────────────+
-| `listen` 1  | M1:L1[0]                                                                      |                  |
-| `add(1)`    | M1:L1[0],M2:L1[1]                                                             |                  |
-| `add(2)`    | M1:L1[0],M2:L1[1, 2]                                                          |                  |
-| `listen` 2  | M1:L1[0],M2:L1[1, 2],M3:L2[2]                                                 |                  |
-| `add(3)`    | M1:L1[0],M2:L1[1,2,3],M3:L2[2],M4:L2[3]                                       |                  |
-| `listen` 3  | M1:L1[0],M2:L1[1,2,3],M3:L2[2],M4:L2[3],M5:L3[3]                              |                  |
-| `add(4)`    | M1:L1[0],M2:L1[1,2,3,4],M3:L2[2],M4:L2[3,4],M5:L3[3],M6:L3[4]                 |                  |
-| `close`     | M1:L1[0],M2:L1[1,2,3,4,DONE],M3:L2[2],M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE]  |                  |
++─────────────+─────────────────────────────────────────────────────────────────────+──────────────────+
+| action      | microtasks                                                          | prints           |
++─────────────+─────────────────────────────────────────────────────────────────────+──────────────────+
+| `listen` 1  | M1:L1[0]                                                            |                  |
+| `add(1)`    | M1:L1[0],M2:L1[1]                                                   |                  |
+| `add(2)`    | M1:L1[0],M2:L1[1, 2]                                                |                  |
+| `listen` 2  | M1:L1[0],M2:L1[1, 2],M3:L2[2]                                       |                  |
+| `add(3)`    | M1:L1[0],M2:L1[1,2,3],M3:L2[2],M4:L2[3]                             |                  |
+| `listen` 3  | M1:L1[0],M2:L1[1,2,3],M3:L2[2],M4:L2[3],M5:L3[3]                    |                  |
+| `add(4)`    | M1:L1[0],M2:L1[1,2,3,4],M3:L2[2],M4:L2[3,4],M5:L3[3],M6:L3[4]       |                  |
+| `close`     | M1:L1[0],M2:L1[1,2,3,4,DONE],M3:L2[2],
+                M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE]                              |                  |
 
-| microtask   | M2:L1[1,2,3,4,DONE],M3:L2[2],M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE]           | `listener 1: 0`  |
-| microtask   | M3:L2[2],M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE],M7:L1[2,3,4,DONE]             | `listener 1: 1`  |
-| microtask   | M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE],M7:L1[2,3,4,DONE]                      | `listener 2: 2`  |
-| microtask   | M5:L3[3],M6:L3[4,DONE],M7:L1[2,3,4,DONE],M8:L2[4,DONE],                       | `listener 2: 3`  |
-| microtask   | M6:L3[4,DONE],M7:L1[2,3,4,DONE],M8:L2[4,DONE],                                | `listener 3: 3`  |
-| microtask   | M7:L1[2,3,4,DONE],M8:L2[4,DONE],M9:L3[DONE]                                   | `listener 3: 4`  |
-| microtask   | M8:L2[4,DONE],M9:L3[DONE],M10:L1[3,4,DONE]                                    | `listener 1: 2`  |
-| microtask   | M9:L3[DONE],M10:L1[3,4,DONE],M11:L2[DONE]                                     | `listener 2: 4`  |
-| microtask   | M10:L1[3,4,DONE],M11:L2[DONE]                                                 |                  |
-| microtask   | M11:L2[DONE],M12:L1[4,DONE]                                                   | `listener 1: 3   |
-| microtask   | M12:L1[4,DONE]                                                                |                  |
-| microtask   | M13:L1[DONE]                                                                  | `listener 1: 4   |
-| microtask   |                                                                               |                  |
-+─────────────+───────────────────────────────────────────────────────────────────────────────+──────────────────+*/
+| microtask   | M2:L1[1,2,3,4,DONE],M3:L2[2],M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE] | `listener 1: 0`  |
+| microtask   | M3:L2[2],M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE],M7:L1[2,3,4,DONE]   | `listener 1: 1`  |
+| microtask   | M4:L2[3,4,DONE],M5:L3[3],M6:L3[4,DONE],M7:L1[2,3,4,DONE]            | `listener 2: 2`  |
+| microtask   | M5:L3[3],M6:L3[4,DONE],M7:L1[2,3,4,DONE],M8:L2[4,DONE],             | `listener 2: 3`  |
+| microtask   | M6:L3[4,DONE],M7:L1[2,3,4,DONE],M8:L2[4,DONE],                      | `listener 3: 3`  |
+| microtask   | M7:L1[2,3,4,DONE],M8:L2[4,DONE],M9:L3[DONE]                         | `listener 3: 4`  |
+| microtask   | M8:L2[4,DONE],M9:L3[DONE],M10:L1[3,4,DONE]                          | `listener 1: 2`  |
+| microtask   | M9:L3[DONE],M10:L1[3,4,DONE],M11:L2[DONE]                           | `listener 2: 4`  |
+| microtask   | M10:L1[3,4,DONE],M11:L2[DONE]                                       |                  |
+| microtask   | M11:L2[DONE],M12:L1[4,DONE]                                         | `listener 1: 3   |
+| microtask   | M12:L1[4,DONE]                                                      |                  |
+| microtask   | M13:L1[DONE]                                                        | `listener 1: 4   |
+| microtask   |                                                                     |                  |
++─────────────+─────────────────────────────────────────────────────────────────────+──────────────────+*/
 }
